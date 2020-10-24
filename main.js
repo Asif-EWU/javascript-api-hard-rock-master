6c9d5584678b126fbe8f26c7ca0c45db6662326d
Asif Mohammad <asifratul45@gmail.com>
2020-10-24 10:44:46 +0600

    added css loader

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let parentDiv = document.getElementById('list-container');
+const parentDiv = document.getElementById('list-container');
 let childDiv;
 let arr; 
 
+
+// handle search input
 const myFunction = () => {
+    if(!parentDiv.innerHTML){
+        // CSS loader animaiton    
+        parentDiv.innerHTML = `<div class="loader mx-auto"></div>`;
+    }
+
     const inputValue = document.getElementById('search-input').value;
     if(!inputValue) {
         parentDiv.innerHTML = "";
@@ -10,40 +17,15 @@ const myFunction = () => {
     }
 
     const url = 'https://api.lyrics.ovh/suggest/' + inputValue;
-    const encodedUrl = encodeURI(url);
+    const encodedUrl = encodeURI(url);      // it handles special characters in the url
 
     fetch(encodedUrl)
-        .then(res => res.json())
-        .then(result => {
-            arr = result.data.slice(0, 10);
-            console.log(arr);
-            displayData(arr);
-        })
-        .catch(error => console.log(error));
-};
-
-const getLyrics = id => {
-    const songInfo = arr.find(item => item.id == id);
-    const lyricsUrl = `https://api.lyrics.ovh/v1/${songInfo.artist}/${songInfo.title}`;
-    const encodedUrl = encodeURI(lyricsUrl);
-    console.log(encodedUrl);
-
-    fetch(encodedUrl)
-        .then(res => res.json())
-        .then(data => displayLyrics(songInfo, data))
-        .catch(error => console.log(error));
-};
-
-const displayLyrics = (songInfo, data) => {
-    console.log(data);
-};
-
-const getDuration = second => {
-    const secondInt = parseInt(second);
-    const minute = Math.floor(secondInt / 60);
-    const remainingSecond = secondInt % 60;
-    if(remainingSecond < 10) return minute + ':0' + remainingSecond;
-    else return minute + ':' + remainingSecond;
+    .then(res => res.json())
+    .then(result => {
+        arr = result.data.slice(0, 10);
+        displayData(arr);
+    })
+    .catch(error => console.log(error));
 };
 
 const displayData = data => {
@@ -64,24 +46,71 @@ const displayData = data => {
     parentDiv.innerHTML = childDiv;
 };
 
+const getDuration = second => {
+    const secondInt = parseInt(second);
+    const minute = Math.floor(secondInt / 60);
+    const remainingSecond = secondInt % 60;
+    if(remainingSecond < 10) return minute + ':0' + remainingSecond;
+    else return minute + ':' + remainingSecond;
+};
+
 const displayListItem = (id, title, artist) => {
     childDiv +=   `<div class="card bg-transparent mb-1">
-                        <div class="card-header lyrics-bg row align-items-center py-4" id="heading${id}">
-                        <div class="col-md-9">
+    <div class="card-header lyrics-bg row align-items-center py-4" id="heading${id}">
+    <div class="col-sm-9">
                             <h3 class="lyrics-name">${title}</h3>
                             <p class="author lead">Album by <span class="font-weight-bold">${artist}</span></p>
                         </div>
                         <div class="col-md-3">
-                            <button id="button${id}" class="btn btn-success text-light" onclick="getLyrics(${id})" type="button" data-toggle="collapse" data-target="#collapse${id}" aria-expanded="true" aria-controls="collapse${id}">
+                        <button class="btn btn-success text-light" onclick="getLyrics(${id})" type="button" data-toggle="collapse" data-target="#collapse${id}" aria-expanded="true" aria-controls="collapse${id}">
                             Get Lyrics
                             </button>
+                            </div>
                         </div>
-                        </div>
-                    
+                        
                         <div id="collapse${id}" class="collapse" aria-labelledby="heading${id}" data-parent="#list-container">
                         <div class="card-body lyrics-bg">
-                            <pre id="lyric${id}" class="lyric"></pre>
-                        </div>
-                        </div>
-                    </div>`
+                            <p>Title: <span class="title font-weight-bold"></span></p>  
+                            <p>Artist: <span class="artist font-weight-bold"></span></p>
+                            <p>Album: <span class="album font-weight-bold"></span></p>
+                            <p>Duration: <span class="duration"></span></p>
+                            <h2 class="text-success mb-4 text-center"><span class="song-name"></span> - <span class="artist-name"></span></h2>
+                            <pre class="lyric text-white text-center"></pre>
+                            </div>
+                            </div>
+                            </div>`
 };
+                        
+
+// handle Get Lyrics button
+const getLyrics = id => {
+    const songInfo = arr.find(item => item.id == id);
+    const lyricsUrl = `https://api.lyrics.ovh/v1/${songInfo.artist}/${songInfo.title}`;
+    const encodedUrl = encodeURI(lyricsUrl);
+    
+    fetch(encodedUrl)
+    .then(res => res.json())
+    .then(data => displayLyrics(songInfo, data))
+        .catch(error => console.log(error));
+};
+    
+const displayLyrics = (songInfo, lyricsData) => {
+    const {id, title, artist, album, duration} = songInfo;
+    let lyrics = lyricsData.lyrics;
+    if(!lyrics)
+    lyrics = "Server Error!! \n Can't load lyrics right now. \n Please try again in a while...";
+    
+    setDetails(id, 'title', title);
+    setDetails(id, 'artist', artist);
+    setDetails(id, 'album', album);
+    setDetails(id, 'duration', duration);    
+    setDetails(id, 'song-name', title);    
+    setDetails(id, 'artist-name', artist);    
+    setDetails(id, 'lyric', lyrics);
+};
+
+const setDetails = (id, className, info) => {
+    const query = `#collapse${id} .${className}`;
+    document.querySelector(query).innerText = info;
+};
+    
